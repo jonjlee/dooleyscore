8715957247d32dd8fc000d696b79a5ff9bfa457f
Jonathan Lee <jonjlee@gmail.com>
2014-10-15 16:42:31 -0500

    Use cancer dx spreadsheet to set cancer flag

--- parse.py
+++ parse.py
@@ -3,13 +3,15 @@
 import sys
 import xlrd
 from collections import namedtuple, OrderedDict
+from datetime import datetime
 import json
+
 from pprint import pprint
 
 # Set up logging
 import logging
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format='%(asctime)-15s: %(message)s')
 
 SheetInfo = namedtuple('SheetInfo', ['dataset_name', 'sheet_name', 'start_row', 'columns'])
 AllSheetInfo = {
@@ -17,19 +19,19 @@ AllSheetInfo = {
         dataset_name='Nipple Discharge',
         sheet_name='NIpple DC',
         start_row=2,
-        columns=['Last name', 'First name', 'sex', 'race', 'DOB', 'age', 'cc', 'Nipple d/c character', 'Hemoccult +/-', 'visit date', 'Mass', 'Axillary LNs', 'Heme Discharge', 'Ducts Involved', 'T4 Findings', 'Total', 'Birads', 'Dx', 'Cancer', 'Patients.Pt ID', 'visits.Pt ID', 'Pt History.Pt ID', 'Notes']
+        columns=['Last name', 'First name', 'sex', 'race', 'DOB', 'age', 'cc', 'Nipple d/c character', 'Hemoccult +/-', 'visit date', 'Mass', 'Axillary LNs', 'Heme Discharge', 'Ducts Involved', 'T4 Findings', 'Total', 'Birads', 'Dx', 'Cancer based on dx', 'Patients.Pt ID', 'visits.Pt ID', 'Pt History.Pt ID', 'Notes']
     ),
     'Mammography': SheetInfo(
         dataset_name='Abnormal Mammography',
         sheet_name='Mammography',
         start_row=2,
-        columns=['Last name', 'First name', 'Sex', 'DOB', 'SSN', 'Notes', 'Visit Date', 'Mass', 'Axillary LNs', 'Heme Discharge', 'Ducts Involved', 'T4 Findings', 'Total', 'Pt ID', 'BIRADS', 'Density/distortion', 'Calcifications', 'Diagnosis', 'Cancer', 'CC']
+        columns=['Last name', 'First name', 'Sex', 'DOB', 'SSN', 'Notes', 'Visit Date', 'Mass', 'Axillary LNs', 'Heme Discharge', 'Ducts Involved', 'T4 Findings', 'Total', 'Pt ID', 'BIRADS', 'Density/distortion', 'Calcifications', 'Diagnosis', 'Cancer based on dx', 'CC']
     ),
     'Br Mass': SheetInfo(
         dataset_name='Breast Mass',
         sheet_name='Br Mass',
         start_row=2,
-        columns=['Last Name', 'First Name', 'DOB', 'Mass Found By', 'Mass Characterization by PennyPacker Exam', 'Visit Date', 'BIRADS', 'Mass', 'Axillary LNs', 'Heme Discharge', 'Ducts Involved', 'T4 Findings', 'Total', 'Patient ID', 'MR Number', 'SSN', 'PreOp Diagnosis', 'PostOp Diagnosis', 'Dx', 'Cancer']
+        columns=['Last Name', 'First Name', 'DOB', 'Mass Found By', 'Mass Characterization by PennyPacker Exam', 'Visit Date', 'BIRADS', 'Mass', 'Axillary LNs', 'Heme Discharge', 'Ducts Involved', 'T4 Findings', 'Total', 'Patient ID', 'MR Number', 'SSN', 'PreOp Diagnosis', 'PostOp Diagnosis', 'Dx', 'Cancer based on dx']
     ),
     'Breast Pain': SheetInfo(
         dataset_name='Breast Pain',
@@ -42,6 +44,7 @@ AllSheetInfo = {
 def parse_data(workbook, sheet_info):
     # Iterate over all sheets
     data = {}
+    date_fields = ['visit date']
     for _, info in sheet_info.items():
         s = workbook.sheet_by_name(info.sheet_name)
 
@@ -54,7 +57,25 @@ def parse_data(workbook, sheet_info):
 
             # Convert row -> dictionary with all lower case field names
             cols = [c.lower() for c in info.columns]
-            rows.append(OrderedDict(zip(cols, row)))
+            row = OrderedDict(zip(cols, row))
+            
+            # Convert date fields to actual dates
+            for f in date_fields:
+                if not f in row: continue
+                try:
+                    date_tuple = xlrd.xldate_as_tuple(row[f], workbook.datemode)
+                    row[f] = datetime(*date_tuple)
+                except:
+                    print('Could not parse Date for %s, %s' % (row['last name'], row['first name']))
+            
+            # # Convert Current Age into a visit date based on DOB
+            # if 'age' in row and 'dob' in row:
+            #     dob = datetime.strptime(row['dob'], '%m/%d/%Y')
+            #     age = int(row['current age'])
+            #     day = 28 if (dob.month == 2 and dob.day == 29) else dob.day
+            #     row['visit date'] = datetime(dob.year + age, dob.month, day)
+
+            rows.append(row)
         
         data[info.dataset_name] = {
             'name': info.dataset_name,
@@ -63,28 +84,51 @@ def parse_data(workbook, sheet_info):
 
     return data
 
-def parse_cancer_pts(workbook):
+def parse_cancer_pts(xlfilename):
     # Open the first worksheet, and extract all first name, last name, and DOB
-    s = workbook.sheet_by_index(0)
-    rows = []
+    wb = xlrd.open_workbook(xlfilename)
+    s = wb.sheet_by_index(0)
+    rows = OrderedDict()
     for i in range(1, s.nrows):
-        row = s.row_values(i, 1, 5)
-        id = '%s;%s;%s' % (row[0],row[1],row[3])
-        rows.append(id.lower())
+        row = s.row_values(i, 0, 6)
+        if not (row[1].strip() and row[2].strip()): continue
 
+        id = '%s;%s;%s' % (row[1],row[2],row[4])
+
+        try:
+            date_tuple = xlrd.xldate_as_tuple(row[5], wb.datemode)
+        except:
+            print('Could not parse visit date: ', row)
+
+        rows[id.lower()] = { 'dx_date': datetime(*date_tuple) }
+
+    wb.release_resources()
     return rows
 
-def set_cancer_field(data, cancer_pts):
+def set_cancer_field(data, xlfilename):
     '''
-    For all patients in cancer_pts (known cancer patients in a list of last;first;dob),
-    set the cancer field for any corresponding entries in data to yes
+    For all patients listed in the excel file, set the cancer field for 
+    any corresponding entries in data to yes
     '''
+    one_year_in_sec = 60*60*24*365
+
+    # Parse the excel sheet into a map of known cancer patients: "last;first;dob" -> {'dx_date'}),
+    cancer_pts = parse_cancer_pts(xlfilename)
     for _, dataset in data.items():
         for row in dataset['data']:
             pt_name_and_dob = '%s;%s;%s' % (row.get('last name'), row.get('first name'), row.get('dob'))
-            if pt_name_and_dob.lower() in cancer_pts and row.get('cancer') != 'yes':
-                logger.info('Changing %s.cancer from %s to yes', pt_name_and_dob, row.get('cancer'))
-                row['cancer'] = 'yes'
+            pt_name_and_dob = pt_name_and_dob.lower()
+            cancer_pt = cancer_pts.get(pt_name_and_dob)
+
+            # Set cancer flag if not already set. Default to no. If patient is 
+            # in excel spreadsheet and visit date is within 1 year prior to dx date,
+            # then set cancer flag to yes.
+            cancer = row.get('cancer') or 'no'
+            if cancer_pt:
+                if 'visit date' in row:
+                    if round((row['visit date'] - cancer_pt['dx_date']).total_seconds() / one_year_in_sec) <= 1:
+                        cancer = 'yes'
+            row['cancer'] = cancer
 
 def remove_phi(data):
     # hpi_fields = set()
@@ -107,12 +151,9 @@ def main(fname, outfile):
     data = parse_data(wb, AllSheetInfo)
     wb.release_resources()
 
-    # Uncomment below to set the cancer field to true for all patients in the cancer patients Excel file
-    #
-    # wb = xlrd.open_workbook('2009 to 2012 breast cancer OUMC.xlsx')
-    # cancer_pts = parse_cancer_pts(wb)
-    # wb.release_resources()
-    # set_cancer_field(data, cancer_pts)
+    # Set the cancer field to true for all patients in the cancer patients Excel files
+    set_cancer_field(data, 'TR data 2 2010.xlsx')
+    set_cancer_field(data, '2009 to 2012 breast cancer OUMC.xlsx')
 
     # Remove all PHI
     remove_phi(data)
@@ -120,11 +161,17 @@ def main(fname, outfile):
 
 def output(data, outfile):
     # Convert to JS
-    js_data = 'data = %s;' % json.dumps(data)
+    dthandler = lambda obj: (
+        obj.isoformat()
+        if isinstance(obj, datetime)
+        else None)
+    js_data = json.dumps(data, default=dthandler)
 
     if not outfile:
         return print(js_data)
-    with open(outfile, 'w') as out:
+    with open(outfile + '.js', 'w') as out:
+        out.write('data = %s;' % js_data)
+    with open(outfile + '.json', 'w') as out:
         out.write(js_data)
 
 if __name__ == '__main__':
